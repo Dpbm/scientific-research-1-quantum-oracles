0bb43bdc8e6d9aa3108f96d01141fa04ce3125e9
Dpbm <dpbm136@gmail.com>
2023-11-28 16:53:13 -0300

    updated toughts about miles and km

--- code/utils.py
+++ code/utils.py
@@ -1,2 +1,10 @@
+import functools
+
 def get_image_path(image_name):
     return f"../images/{image_name}"
+
+@functools.lru_cache(maxsize=None)
+def fibonacci(n):
+    if(n <= 1):
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2) 
